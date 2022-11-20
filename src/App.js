36e251e668f290e43e9d3b073f1d50a140e7a6b3
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2022-11-20 10:44:34 -0500

    39 input search

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("constructor");
   }
   // When do I get the list
   // How do I get the list
   // where to put the list
 
   componentDidMount() {
+    console.log("componentDidMount");
+
     // code will be executed whenever the component will mount
     // it only happens once
     // fetch for getting data from json api
@@ -33,8 +36,16 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
+
     return (
       <div className="App">
+        <input
+          className="search"
+          type="search"
+          placeholder="search monster"
+          onChange={() => {}}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
