17cebb500565e0ea16510c0ad95dadce2bc25214
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2022-11-20 04:46:54 -0500

    class 36

--- src/App.js
+++ src/App.js
@@ -7,26 +7,34 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        { id: 1, name: "Kohli" },
-        { id: 2, name: "Shaheen" },
-        { id: 3, name: "Butler" },
-      ],
+      monsters: [],
     };
+  }
+  // When do I get the list
+  // How do I get the list
+  // where to put the list
+
+  componentDidMount() {
+    // code will be executed whenever the component will mount
+    // it only happens once
+    // fetch for getting data from json api
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log("Monsters has been updated");
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
-        {/* <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
-
-        What if we have million records 
-        
-        with array mapping
-        */}
-
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
@@ -40,37 +48,3 @@ class App extends Component {
 }
 
 export default App;
-
-/*
-  21 - 31
-  <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            this is {this.state.name} {this.state.cast}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return { name: "Ameer Hamza" };
-                },
-                () => {
-                  console.log(this.state.name);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-
-    
-*/
