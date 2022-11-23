d15176f86552a80efdc110a905f6c5ea2df3aebc
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2022-11-23 00:43:52 -0500

    50 CSS CardList

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,7 +7,7 @@ class Card extends Component {
     const { id, name, email } = monster;
 
     return (
-      <div className="card-container ">
+      <div className="card-container " key={id}>
         <img
           alt={`monster ${name}`}
           src={`https://robohash.org/${id}?set=set2&size=180x180`}
